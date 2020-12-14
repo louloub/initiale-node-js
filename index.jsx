e7b465bab68f3e19e7afe0a6f50a07790bd49dd3
loulou <yourdj.france@gmail.com>
2020-12-14 12:04:18 +0100

    challenge JS TDD 1 Introduction

--- index.jsx
+++ index.jsx
@@ -1,7 +1,5 @@
 const connection = require("./src/config");
 const express = require("express");
-// const movies = require("./movies");
-// const users = require("./users");
 
 const port = 3000;
 const app = express();
@@ -15,17 +13,62 @@ function logInfos(req, res, next) {
 
 app.use(logInfos); 
 
-
+// Port listen
 app.listen(port, () => {
     console.log(`Server is runing on ${port}`);
   });
+
+// Connection  
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
   
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
+// --- TDD ---
+
+const assert = require('assert');
+
+// --- MY TESTING FUNCTION ---
+function capitalizeFirstLetters(input){
+  if (input.length > 0 && !input.includes(" ")) {
+    return input[0].toUpperCase() + input.slice(1)
+  } else if (input === "") {
+    console.log("----------- in first elsif")
+    return ""
+  } else if (input.length > 0 && input.includes(" ")) {
+    let wordsArray = input.split(" ");
+    for (let i = 0; i < wordsArray.length; i++) {
+      wordsArray[i] = wordsArray[i][0].toUpperCase() + wordsArray[i].substr(1);
     }
-  
-    console.log('connected as id ' + connection.threadId);
-  });
-  
+    return wordsArray.join(' ');
+  }
+}
+assert.strictEqual(capitalizeFirstLetters(''), '');
+assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
+assert.strictEqual(capitalizeFirstLetters('javaScript cest trop cool'), 'JavaScript Cest Trop Cool');
+
+// --- testing function example ---
+function capitalizeFirst(input) {
+  return input.length > 0
+    ? input[0].toUpperCase() + input.slice(1)
+    : '';
+}
+
+// Check that capitalizeFirst is a function
+assert.strictEqual(typeof capitalizeFirst, 'function');
+
+// Check that capitalizeFirst accepts one argument
+assert.strictEqual(capitalizeFirst.length, 1);
+
+// Check that capitalizeFirst transforms javaScript correctly
+assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
+
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirst('z'), 'Z');
+
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirst(''), '');
