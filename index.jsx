d046672e412d40a46ab8c7b491e83bd8ac145ba9
loulou <yourdj.france@gmail.com>
2021-02-03 13:49:29 +0100

    first route works with postman

--- index.jsx
+++ index.jsx
@@ -1,12 +1,11 @@
 // index.js
 const app = require('./app');
 
-const port = process.env.port || 3000;
+const port = process.env.SERVER_PORT;
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
   if (err) {
     throw new Error(`An error occurred: ${err.message}`);
   }
   console.log(`Server is listening on ${port}`);
 });
-
