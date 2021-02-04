41ead89ef92ef720af6012174ecfb7e4aef8bfd0
loulou <yourdj.france@gmail.com>
2021-02-04 08:25:58 +0100

    add delete works on database

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// DELETE crytpo
+app.delete("/cryptolist/:cryptoId", async (request, response) => {
+  try {
+    const { cryptoId } = request.params;
+    await connection.query("DELETE FROM crypto WHERE id=?", [cryptoId]);
+    response.sendStatus(204);
+  } catch (err) {
+    console.log(err);
+    response.status(500).send("😱 Error delete crypto");
+  }
+});
+
 // GET - Retrieve all of the data from your table
 app.get("/cryptolist", (request, response) => {
   console.log("in");
@@ -23,20 +35,33 @@ app.get("/cryptolist", (request, response) => {
   }
 });
 
-// WORKING ON = actualy can actualise price of bitcoin on database from 
-// COINGECKO online price
-app.post(`/cryptolist`, async (request, response) => {
-  console.log("request.body ==> ", request.body);
-  console.log("request.body ID ==> ", request.body.id);
-
+// POST new crypto in db
+app.post(`/cryptolist/newCrypto`, async (request, response) => {
+  console.log("newCrypto request.body ==> ", request.body.newCoin);
   try {
     const {
-      id: id,
-      actualPrice: actualPrice,
-    } = request.body;
+      name,
+      coin,
+      type,
+      description
+    } = request.body.newCoin
+    console.log(name)
+    const addNewCrypto = await connection.query(
+      `INSERT INTO crypto SET ?`,
+      [{name,coin,type,description}]
+    );
+  } catch (err) {
+    console.log(err)
+  }
+});
+
+// POST Actualise crypto price with COINGECKO online price
+app.post(`/cryptolist`, async (request, response) => {
+  try {
+    const { id: id, actualPrice: actualPrice } = request.body;
     const cryptoResult = await connection.query(
       `UPDATE crypto SET actualPrice = ${actualPrice} WHERE id = ${id}`,
-      [{ actualPrice}]
+      [{ actualPrice }]
     );
 
     return response.status(200).json({
@@ -49,36 +74,5 @@ app.post(`/cryptolist`, async (request, response) => {
     console.log(err);
   }
 });
-
-// DONT WORK PUT - modifie actualPrice
-// app.put(`/changeActualPrice/:cryptoId`, async (request, response) => {
-//   response.send("in put request");
-//   try {
-//     const { cryptoId } = req.params;
-//     await connection.query("UPDATE crypto SET ? where id=?", [
-//       { ...req.body },
-//       cryptoId
-//     ]);
-//     res.status(200).json({ id: cryptoId, ...req.body });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).send("error modifying crypto");
-//   }
-// });
-
-// DONT WORK POST - Retrieve all of the data from your table
-// app.post(`/crypto/actualPrice/:id`, (request, response) => {
-//   const id = request.params;
-//   console.log("in", id);
-//   try {
-//     connection.query(`SELECT * FROM crypto`, (err, results) => {
-//       console.log("result ==> ", results);
-//       response.status(200).json(results);
-//     });
-//   } catch (err) {
-//     response.status(404).json({ error: "Crypto not found" });
-//     console.log("err => ", err);
-//   }
-// });
 
 module.exports = app;
