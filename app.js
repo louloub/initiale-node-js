c5f8e1a84df1bf6b4a4a3b93f9735ad8b6118373
Malécot <harold.malecot@gmail.com>
2021-06-20 18:56:46 +0200

    create edit delete works

--- app.js
+++ app.js
@@ -9,18 +9,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// DELETE crytpo
-app.delete("/cryptolist/:cryptoId", async (request, response) => {
-  try {
-    const { cryptoId } = request.params;
-    await connection.query("DELETE FROM crypto WHERE id=?", [cryptoId]);
-    response.sendStatus(204);
-  } catch (err) {
-    console.log(err);
-    response.status(500).send("😱 Error delete crypto");
-  }
-});
-
 // GET - Retrieve all of the data from your table
 app.get("/cryptoList", (request, response) => {
   try {
@@ -33,15 +21,13 @@ app.get("/cryptoList", (request, response) => {
   }
 });
 
-// POST new crypto in db
+// POST - new crypto in db
 app.post(`/cryptolist/newCrypto`, async (request, response) => {
-  console.log("--- in new crypto ---");
-  console.log("--- request.body.newCoin ==> ", request.body.newCoin);
   try {
     console.log("BACK in TRY ");
-    const { name, coin, type, description } = request.body.newCoin;
+    const { name, coin, type, description, actualPrice, marketCap } = request.body.newCoin;
     await connection.query(`INSERT INTO crypto SET ?`, [
-      { name, coin, type, description }
+      { name, coin, type, description, actualPrice, marketCap }
     ]);
     console.log("BACK in end TRY ");
   } catch (err) {
@@ -49,16 +35,14 @@ app.post(`/cryptolist/newCrypto`, async (request, response) => {
   }
 });
 
-// POST - Actualise crypto price with COINGECKO online price
-app.post(`/cryptoListPrice`, async (request, response) => {
+// POST - Actualise crypto price and marketcap with COINGECKO online price
+app.post(`/updateCrypto`, async (request, response) => {
   try {
     const {
       id: id,
       actualPrice: actualPrice,
       marketCap: marketCap
     } = request.body;
-    console.log("BACK cryptoId ==> ", id);
-    console.log("BACK actualPrice ==> ", actualPrice);
     const cryptoResult = await connection.query(
       `UPDATE crypto SET actualPrice = ${actualPrice}, marketCap = ${marketCap} WHERE id = ${id}`,
       [{ actualPrice }]
@@ -75,25 +59,15 @@ app.post(`/cryptoListPrice`, async (request, response) => {
   }
 });
 
-// POST Actualise crypto market cap with COINGECKO online price
-app.post(`/cryptoListMarketCap`, async (request, response) => {
+// DELETE crytpo
+app.delete("/cryptolist/:cryptoId", async (request, response) => {
   try {
-    const { id: id, marketCap: marketCap } = request.body;
-    console.log("BACK cryptoId ==> ", id);
-    console.log("BACK marketCap ==> ", marketCap);
-    const cryptoResult = await connection.query(
-      `UPDATE crypto SET marketCap = ${marketCap} WHERE id = ${id}`,
-      [{ marketCap }]
-    );
-
-    return response.status(200).json({
-      data: {
-        marketCap
-      },
-      message: "🎉 Successfully saved crypto list with new market cap"
-    });
+    const { cryptoId } = request.params;
+    await connection.query("DELETE FROM crypto WHERE id=?", [cryptoId]);
+    response.sendStatus(204);
   } catch (err) {
     console.log(err);
+    response.status(500).send("😱 Error delete crypto");
   }
 });
 
